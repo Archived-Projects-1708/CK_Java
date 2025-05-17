e90e50c95b3380e72b0cfb35305ea2203c1b7e05
hahuy1708 <hahuy17082005@gmail.com>
2025-05-17 16:50:09 +0700

    add QuestionModel

--- src/main/java/com/example/exambank/model/Question.java
+++ src/main/java/com/example/exambank/model/Question.java
@@ -1,120 +1,52 @@
 package com.example.exambank.model;
 
+import javax.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
+@Table(name = "questions")
 public class Question {
-    private int questionId;
-    private String content;
-    private int typeId;
-    private int levelId;
-    private String audioPath;
-    private LocalDateTime createdDate;
-    private LocalDateTime updatedDate;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(columnDefinition = "NVARCHAR(MAX)", nullable = false)
+    private String content;
+
+    @Column(columnDefinition = "NVARCHAR(MAX)", nullable = false)
+    private String answer;
+
+    @Column(name = "ai_answer_hint", columnDefinition = "NVARCHAR(MAX)")
+    private String aiAnswerHint;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ExamQuestion> examQuestions = new ArrayList<>();
+
+    // Constructors
     public Question() {}
 
-    public Question(int questionId,
-                    String content,
-                    int typeId,
-                    int levelId,
-                    String audioPath,
-                    LocalDateTime createdDate,
-                    LocalDateTime updatedDate) {
-        this.questionId = questionId;
+    public Question(String content, String answer, String aiAnswerHint) {
         this.content = content;
-        this.typeId = typeId;
-        this.levelId = levelId;
-        this.audioPath = audioPath;
-        this.createdDate = createdDate;
-        this.updatedDate = updatedDate;
+        this.answer = answer;
+        this.aiAnswerHint = aiAnswerHint;
     }
-
-    public int getQuestionId() {
-        return questionId;
-    }
-
-    public void setQuestionId(int questionId) {
-        this.questionId = questionId;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public int getTypeId() {
-        return typeId;
-    }
-
-    public void setTypeId(int typeId) {
-        this.typeId = typeId;
-    }
-
-    public int getLevelId() {
-        return levelId;
-    }
-
-    public void setLevelId(int levelId) {
-        this.levelId = levelId;
-    }
-
-    public String getAudioPath() {
-        return audioPath;
-    }
-
-    public void setAudioPath(String audioPath) {
-        this.audioPath = audioPath;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public LocalDateTime getUpdatedDate() {
-        return updatedDate;
-    }
-
-    public void setUpdatedDate(LocalDateTime updatedDate) {
-        this.updatedDate = updatedDate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Question)) return false;
-        Question that = (Question) o;
-        return questionId == that.questionId &&
-                typeId == that.typeId &&
-                levelId == that.levelId &&
-                Objects.equals(content, that.content) &&
-                Objects.equals(audioPath, that.audioPath) &&
-                Objects.equals(createdDate, that.createdDate) &&
-                Objects.equals(updatedDate, that.updatedDate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(questionId, content, typeId, levelId, audioPath, createdDate, updatedDate);
-    }
-
-    @Override
-    public String toString() {
-        return "Question{" +
-                "questionId=" + questionId +
-                ", content='" + content + '\'' +
-                ", typeId=" + typeId +
-                ", levelId=" + levelId +
-                ", audioPath='" + audioPath + '\'' +
-                ", createdDate=" + createdDate +
-                ", updatedDate=" + updatedDate +
-                '}';
-    }
+    public List<ExamQuestion> getExamQuestions() { return examQuestions; }
+    public void setExamQuestions(List<ExamQuestion> examQuestions) { this.examQuestions = examQuestions; }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+    public String getAnswer() { return answer; }
+    public void setAnswer(String answer) { this.answer = answer; }
+    public String getAiAnswerHint() { return aiAnswerHint; }
+    public void setAiAnswerHint(String aiAnswerHint) { this.aiAnswerHint = aiAnswerHint; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
 }
+
